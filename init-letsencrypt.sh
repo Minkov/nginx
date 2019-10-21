5804a894a526ff2f7017842235e9f7f0895c527e
Doncho Minkov <DonchoMinkov@gmail.com>
2019-10-21 15:23:51 +0300

    Fixed domains

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(interactive.serveo.net www.interactive.serveo.net ro.interactive.serveo.net)
+domains=(interactive-test.tk www.interactive-test.tk ro.interactive-test.tk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
