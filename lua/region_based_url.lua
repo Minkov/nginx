6e9f5ddbd052b63b7e63ea940cd02ad477f96700
Doncho Minkov <DonchoMinkov@gmail.com>
2019-10-31 16:08:19 +0200

    More fixes on the redirects

--- lua/region_based_url.lua
+++ lua/region_based_url.lua
@@ -1,5 +1,8 @@
 if ngx.var.region == 'RO' and ngx.var.name ~= 'ro'
 then
     return ngx.redirect("https://ro.interactive.serveo.net", ngx.HTTP_MOVED_TEMPORARILY)
+elseif ngx.var.region == 'BG' and ngx.var.name == 'ro'
+then
+    return ngx.redirect("https://interactive.serveo.net", ngx.HTTP_MOVED_TEMPORARILY)
 end
 -- ngx.exit(ngx.OK)
