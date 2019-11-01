4a3ed24fea93974a9fbda2e6e8a9f02d1e4276e9
Doncho Minkov <DonchoMinkov@gmail.com>
2019-11-01 09:55:59 +0200

    Moved configs to production folder https://github.com/SoftUni-Internal/interactive-issues/issues/1026

--- production/lua/region_based_url.lua
+++ production/lua/region_based_url.lua
@@ -1,7 +1,7 @@
 if ngx.var.region == 'RO' and ngx.var.name ~= 'ro'
 then
     return ngx.redirect("https://ro.softuni.org" .. ngx.var.request_uri, ngx.HTTP_MOVED_TEMPORARILY)
-elseif ngx.var.region == 'BG' and ngx.var.name == 'ro'
+elseif ngx.var.region ~= 'RO' and ngx.var.name == 'ro'
 then
     return ngx.redirect("https://softuni.org" .. ngx.var.request_uri, ngx.HTTP_MOVED_TEMPORARILY)
 end
