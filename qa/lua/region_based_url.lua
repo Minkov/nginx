a00a53fa1e65f9f74ea207be114657aefc6bf0c0
admins <admins@localhost.localdomain>
2020-02-17 11:06:44 +0200

    Added QA environment configuration https://github.com/SoftUni-Internal/interactive-issues/issues/1330

---
+++ qa/lua/region_based_url.lua
@@ -0,0 +1,8 @@
+-- if ngx.var.region == 'RO' and ngx.var.name ~= 'ro'
+-- then
+--     return ngx.redirect("https://ro.softuni.org" .. ngx.var.request_uri, ngx.HTTP_MOVED_TEMPORARILY)
+-- elseif ngx.var.region ~= 'RO' and ngx.var.name == 'ro'
+-- then
+--     return ngx.redirect("https://softuni.org" .. ngx.var.request_uri, ngx.HTTP_MOVED_TEMPORARILY)
+-- end
+-- ngx.exit(ngx.OK)
