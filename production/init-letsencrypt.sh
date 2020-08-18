16e423340f65ff6adfc6b064f7fe8592d10c3dbb
Georgi Georgiev <gogo4ds@gmail.com>
2020-08-18 11:11:02 +0300

    Added configurations for South Africa (co.za) domain

--- production/init-letsencrypt.sh
+++ production/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(softuni.org ro.softuni.org auth.softuni.org admin.softuni.org)
+domains=(softuni.org ro.softuni.org auth.softuni.org admin.softuni.org softuni.co.za)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="donchominkov@gmail.com" # Adding a valid address is strongly recommended
